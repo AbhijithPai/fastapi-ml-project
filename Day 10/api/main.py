f8213077a39eb91177a97d661adf89fddcfaa31f
Abhijith Pai <123663491+AbhijithPai@users.noreply.github.com>
2025-12-13 13:25:02 +0530

    Delete Day 10 directory

--- Day 10/api/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from api.routes import router
-
-app = FastAPI(title="Sentiment API")
-app.include_router(router)
--- Day 10/api/schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class InputData(BaseModel):
-    text: str
-
-class SentimentResponse(BaseModel):
-    sentiment: str
